c160c2acab808588a7a037298560a484f132ee7a
Jwell2014 <91597225+Jwell2014@users.noreply.github.com>
2023-02-20 21:08:07 +0100

    création card par type

--- src/component/CardPokemon.js
+++ src/component/CardPokemon.js
@@ -17,6 +17,56 @@ export default function CardPokemon(props) {
     console.log("top");
   }
 
+  // Permet un affichage de la date JJ/MM/AAAA
+  const formatDate = (date) => {
+    return `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`;
+  };
+
+  const formatType = (type) => {
+    let color;
+
+    switch (type) {
+      case "Feu":
+        color = "red lighten-1";
+        break;
+      case "Eau":
+        color = "blue lighten-1";
+        break;
+      case "Plante":
+        color = "green lighten-1";
+        break;
+      case "Insecte":
+        color = "brown lighten-1";
+        break;
+      case "Normal":
+        color = "grey lighten-3";
+        break;
+      case "Vol":
+        color = "blue lighten-3";
+        break;
+      case "Poison":
+        color = "deep-purple accent-1";
+        break;
+      case "Fée":
+        color = "pink lighten-4";
+        break;
+      case "Psy":
+        color = "deep-purple darken-2";
+        break;
+      case "Electrik":
+        color = "lime accent-1";
+        break;
+      case "Combat":
+        color = "deep-orange";
+        break;
+      default:
+        color = "grey";
+        break;
+    }
+
+    return `chip ${color}`;
+  };
+
   return (
     <>
       <div
@@ -33,8 +83,13 @@ export default function CardPokemon(props) {
             <div className="card-content">
               <p>Nom{pokemon.name}</p>
               <p>
-                <small>{pokemon.created.toString()}</small>
+                <small>{formatDate(pokemon.created)}</small>
               </p>
+              {pokemon.types.map((type) => (
+                <span key={type} className={formatType(type)}>
+                  {type}
+                </span>
+              ))}
             </div>
           </div>
         </div>
