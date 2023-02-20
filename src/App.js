bd43c1952c0acf1cdb9ba61b1efe34f6c111cdec
Jwell2014 <91597225+Jwell2014@users.noreply.github.com>
2023-02-20 17:30:21 +0100

    affichage liste pokemon

--- src/App.js
+++ src/App.js
@@ -12,6 +12,30 @@ export default function App() {
     <div>
       <h1>pokedex</h1>
       <p>Vous avez {pokemons.length} POKEMON dans votre sac</p>
+      <div>
+        <h1 className="center">Pokédex</h1>
+        <div className="container">
+          <div className="row">
+            {pokemons.map(({ id, name, picture, created }) => (
+              <div className="col s6 m4" key={id}>
+                <div className="card horizontal">
+                  <div className="card-image">
+                    <img src={picture} alt={name} />
+                  </div>
+                  <div className="card-stacked">
+                    <div className="card-content">
+                      <p>{name}</p>
+                      <p>
+                        <small>{created.toString()}</small>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
